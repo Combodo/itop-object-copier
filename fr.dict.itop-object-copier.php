c8d88be4b02883e03a27ab6c7fa547279bda6ba0
Romain Quetiez <romain.quetiez@combodo.com>
2014-02-18 11:10:14 +0000

    Added french translation
    SVN:trunk[3320]

---
+++ fr.dict.itop-object-copier.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Localized data
+ *
+ * @copyright   Copyright (C) 2013 XXXXX
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+Dict::Add('FR FR', 'French', 'Français', array(
+	// Dictionary entries go here
+	'object-copier:menu_label:default' => 'Cloner...',
+	'object-copier:form_label:default' => 'Clonage de %1$s',
+	'object-copier:report_label:default' => 'Cloné depuis %1$s',
+));
+?>
