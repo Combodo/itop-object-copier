662fd497c55e1832c6c5855c29802455e8ed1a5f
Romain Quetiez <romain.quetiez@combodo.com>
2017-10-06 09:45:46 +0000

    Releasing 1.1.9
    SVN:trunk[6173]

--- module.itop-object-copier.php
+++ module.itop-object-copier.php
@@ -5,7 +5,7 @@
 
 SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-	'itop-object-copier/1.1.8',
+	'itop-object-copier/1.1.9',
 	array(
 		// Identification
 		//
