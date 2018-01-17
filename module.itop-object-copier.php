5da63a471f94ceac1cdbc18e3d764eea3c2e5f00
Pierre Goiffon <pierre.goiffon@combodo.com>
2018-01-17 08:51:10 +0000

    Preparing 2.4.1 : update trunk modules versions
    SVN:trunk[6389]

--- module.itop-object-copier.php
+++ module.itop-object-copier.php
@@ -5,7 +5,7 @@
 
 SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-	'itop-object-copier/1.1.10',
+	'itop-object-copier/1.2.0',
 	array(
 		// Identification
 		//
@@ -15,7 +15,7 @@ SetupWebPage::AddModule(
 		// Setup
 		//
 		'dependencies' => array(
-			
+
 		),
 		'mandatory' => false,
 		'visible' => true,
@@ -26,7 +26,7 @@ SetupWebPage::AddModule(
 			'main.itop-object-copier.php'
 		),
 		'webservice' => array(
-			
+
 		),
 		'data.struct' => array(
 			// add your 'structure' definition XML files here,
@@ -34,11 +34,11 @@ SetupWebPage::AddModule(
 		'data.sample' => array(
 			// add your sample data XML files here,
 		),
-		
+
 		// Documentation
 		//
 		'doc.manual_setup' => '', // hyperlink to manual setup documentation, if any
-		'doc.more_information' => '', // hyperlink to more information, if any 
+		'doc.more_information' => '', // hyperlink to more information, if any
 
 		// Default settings
 		//
