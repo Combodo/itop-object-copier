ac03244ff0a4126c0cb0f7a25b4452de847a6008
Molkobain <lajarige.guillaume@free.fr>
2018-11-02 11:21:25 +0100

    Update dictionaries for all languages to iTop 2.6

--- es_cr.dict.itop-object-copier.php
+++ es_cr.dict.itop-object-copier.php
@@ -5,8 +5,7 @@
  * @copyright   Copyright (C) 2013 XXXXX
  * @license     http://opensource.org/licenses/AGPL-3.0
  */
-
-Dict::Add('ES CR', 'Spanish', 'Español, Castellano', array(
+Dict::Add('ES CR', 'Spanish', 'Español, Castellaño', array(
 	// Dictionary entries go here
 	'object-copier:menu_label:default' => 'Clonar...',
 	'object-copier:form_label:default' => 'Clonando %1$s',
