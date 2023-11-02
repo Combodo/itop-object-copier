d4f6866cbdf9318153186ad19659a2a1af8c116d
Devin <bdejin@hotmail.com>
2023-11-02 16:31:22 +0800

    N°6893 - Update chinese translations thanks to @bdejin (#5)

--- zh_cn.dict.itop-object-copier.php
+++ zh_cn.dict.itop-object-copier.php
@@ -22,10 +22,10 @@
  */
 Dict::Add('ZH CN', 'Chinese', '简体中文', array(
 	// Dictionary entries go here
-	'object-copier:menu_label:default' => 'Clone...~~',
-	'object-copier:form_label:default' => 'Cloning %1$s~~',
-	'object-copier:report_label:default' => 'Cloned from %1$s~~',
+	'object-copier:menu_label:default' => '克隆...',
+	'object-copier:form_label:default' => '正在克隆%1$s',
+	'object-copier:report_label:default' => '克隆于%1$s',
 
-	'object-copier:error:preset' => 'An error has been encountered while presetting the object to create: %1$s. Please contact your administrator.~~',
-	'object-copier:error:retrofit' => 'An error has been encountered while retrofitting some information back to the source object: %1$s. Please contact your administrator.~~',
+	'object-copier:error:preset' => '预调此待创建对象时产生了错误: %1$s. 请联系管理员.',
+	'object-copier:error:retrofit' => '向此源对象回传更新信息时产生了错误: %1$s. 请联系管理员.',
 ));
