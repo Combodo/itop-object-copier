5fd2ebcafc6dd8026b9f3d7da7a5952d4fc8df76
Timothee <timothee.callet@combodo.com>
2024-06-11 16:59:01 +0200

    Automated dictionaries synchronization with english reference

--- zh_cn.dict.itop-object-copier.php
+++ zh_cn.dict.itop-object-copier.php
@@ -2,30 +2,17 @@
 /**
  * Localized data
  *
- * @copyright Copyright (C) 2010-2018 Combodo SARL
- * @license	http://opensource.org/licenses/AGPL-3.0
- *
- * This file is part of iTop.
- *
- * iTop is free software; you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * iTop is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with iTop. If not, see <http://www.gnu.org/licenses/>
+ * @copyright Copyright (C) 2010-2024 Combodo SAS
+ * @license    https://opensource.org/licenses/AGPL-3.0
+ * 
  */
-Dict::Add('ZH CN', 'Chinese', '简体中文', array(
-	// Dictionary entries go here
-	'object-copier:menu_label:default' => '克隆...',
-	'object-copier:form_label:default' => '正在克隆%1$s',
-	'object-copier:report_label:default' => '克隆于%1$s',
-
+/**
+ *
+ */
+Dict::Add('ZH CN', 'Chinese', '简体中文', [
 	'object-copier:error:preset' => '预调此待创建对象时产生了错误: %1$s. 请联系管理员.',
 	'object-copier:error:retrofit' => '向此源对象回传更新信息时产生了错误: %1$s. 请联系管理员.',
-));
+	'object-copier:form_label:default' => '正在克隆%1$s',
+	'object-copier:menu_label:default' => '克隆...',
+	'object-copier:report_label:default' => '克隆于%1$s',
+]);
