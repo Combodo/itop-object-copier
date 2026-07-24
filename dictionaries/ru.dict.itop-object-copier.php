f00c1d2f342e174155157acc3dfb420f4d681973
Molkobain <lajarige.guillaume@free.fr>
2026-07-24 21:23:05 +0200

    Update dictionaries/ru.dict.itop-object-copier.php

--- dictionaries/ru.dict.itop-object-copier.php
+++ dictionaries/ru.dict.itop-object-copier.php
@@ -10,9 +10,9 @@
  *
  */
 Dict::Add('RU RU', 'Russian', 'Русский', [
-	'object-copier:error:preset' => 'Ошибка при установке начальных значений объекта: %1$s. Пожалуйста свяжитесь с Вашим администратором.~~',
-	'object-copier:error:retrofit' => 'Не удалось обновить исходный объект: %1$s. Пожалуйста свяжитесь с Вашим администратором.~~',
-	'object-copier:form_label:default' => 'Клонировать %1$s~~',
-	'object-copier:menu_label:default' => 'Клонировать...~~',
-	'object-copier:report_label:default' => 'Клонировать из %1$s~~',
+	'object-copier:error:preset' => 'Ошибка при установке начальных значений объекта: %1$s. Пожалуйста свяжитесь с Вашим администратором.',
+	'object-copier:error:retrofit' => 'Не удалось обновить исходный объект: %1$s. Пожалуйста свяжитесь с Вашим администратором.',
+	'object-copier:form_label:default' => 'Клонировать %1$s',
+	'object-copier:menu_label:default' => 'Клонировать...',
+	'object-copier:report_label:default' => 'Клонировать из %1$s',
 ]);
