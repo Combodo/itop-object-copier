c6ea1304bea58e8006fa7e044ced2c3158e3a190
Konstantin Nikulin <apollo2k4@narod.ru>
2026-07-31 14:57:27 +0300

    N°9874 - Update russian translations (#11)
    * add RU dict
    
    * Update dictionaries/ru.dict.itop-object-copier.php
    
    ---------
    
    Co-authored-by: Molkobain <lajarige.guillaume@free.fr>

--- dictionaries/ru.dict.itop-object-copier.php
+++ dictionaries/ru.dict.itop-object-copier.php
@@ -10,9 +10,9 @@
  *
  */
 Dict::Add('RU RU', 'Russian', 'Русский', [
-	'object-copier:error:preset' => 'An error has been encountered while presetting the object to create: %1$s. Please contact your administrator.~~',
-	'object-copier:error:retrofit' => 'An error has been encountered while retrofitting some information back to the source object: %1$s. Please contact your administrator.~~',
-	'object-copier:form_label:default' => 'Cloning %1$s~~',
-	'object-copier:menu_label:default' => 'Clone...~~',
-	'object-copier:report_label:default' => 'Cloned from %1$s~~',
+	'object-copier:error:preset' => 'Ошибка при установке начальных значений объекта: %1$s. Пожалуйста свяжитесь с Вашим администратором.',
+	'object-copier:error:retrofit' => 'Не удалось обновить исходный объект: %1$s. Пожалуйста свяжитесь с Вашим администратором.',
+	'object-copier:form_label:default' => 'Клонировать %1$s',
+	'object-copier:menu_label:default' => 'Клонировать...',
+	'object-copier:report_label:default' => 'Клонировать из %1$s',
 ]);
